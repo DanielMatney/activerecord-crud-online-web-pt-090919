f27eadae4f224ba86213d856f639cf5ed22be052
Daniel Matney <danieljmatney@gmail.com>
2019-12-12 03:11:24 +0000

    Automatically backed up by Learn

--- db/migrate/20191212030859_create_movies.rb
+++ db/migrate/20191212030859_create_movies.rb
@@ -1,9 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    add_column 
-    
-    
-    
-    
+    add_column :movies, :title, :string
+    add_column :movies, :release_date, :datetime
+    add_column :movies, :director, :string
+    add_column :movies, :lead, :string
+    add_column :movies, :in_theaters, :boolean
   end
 end
